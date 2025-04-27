d2700b7794bf660b7f618e211229a5058c3a42b1
Alex-Yu21 <yualex2112@gmail.com>
2025-04-27 09:24:43 +0400

    style: add app colors class for fixed colors in both themes (dark/light)

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color tertiary100 = Color(0xFFFFFFFF);
+  static const Color tertiary99 = Color(0xFFFFFBFF);
+  static const Color tertiary98 = Color(0xFFFFF8F9);
+  static const Color tertiary95 = Color(0xFFFFECF4);
+  static const Color tertiary90 = Color(0xFFFFD8EE);
+  static const Color tertiary80 = Color(0xFFE8B9D5);
+  static const Color tertiary70 = Color(0xFFCB9EB9);
+  static const Color tertiary60 = Color(0xFFAE849E);
+  static const Color tertiary50 = Color(0xFF936B84);
+  static const Color tertiary40 = Color(0xFF78536B);
+  static const Color tertiary35 = Color(0xFF6B475E);
+  static const Color tertiary30 = Color(0xFF5E3C52);
+  static const Color tertiary25 = Color(0xFF523147);
+  static const Color tertiary20 = Color(0xFF46263B);
+  static const Color tertiary15 = Color(0xFF3A1B30);
+  static const Color tertiary10 = Color(0xFF2E1126);
+  static const Color tertiary5 = Color(0xFF22071B);
+}
