a6439c7f7ab53c524ce7d317bef0b36e7a46721c
Alex-Yu21 <yualex2112@gmail.com>
2025-05-02 20:11:40 +0400

    test: new test for status overview widget

--- test/features/home/widgets/status_overview_widget_test.dart
+++ test/features/home/widgets/status_overview_widget_test.dart
@@ -1,24 +1,28 @@
+import 'package:flashcards/features/home/cubit/status_overview_cubit.dart';
 import 'package:flashcards/features/home/presentation/widgets/status_overview_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('StatusOverviewWidget', () {
-    testWidgets('should display all labels and counters', (
-      WidgetTester tester,
-    ) async {
+    testWidgets('shoul displays all labels and counters', (tester) async {
+      final cubit =
+          StatusOverviewCubit()
+            ..update(newWords: 5, learning: 3, reviewing: 7, mastered: 2);
+
       await tester.pumpWidget(
-        const MaterialApp(
+        MaterialApp(
           home: Scaffold(
-            body: StatusOverviewWidget(
-              newWords: 5,
-              learning: 3,
-              reviewing: 7,
-              mastered: 2,
+            body: BlocProvider.value(
+              value: cubit,
+              child: const StatusOverviewWidget(),
             ),
           ),
         ),
       );
+
+      await tester.pump(const Duration(seconds: 1));
 
       expect(find.text('New Words'), findsOneWidget);
       expect(find.text('Learning'), findsOneWidget);
