e4a402cddc3cc3d2d8e07e58ef3cea7fdd19bd63
Alex-Yu21 <yualex2112@gmail.com>
2025-05-11 13:00:11 +0400

    refactor: extract navBar from home page

---
+++ lib/features/tabs/presentation/widgets/app_nav_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flashcards/core/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppBottomNavBar extends StatelessWidget {
+  const AppBottomNavBar({
+    super.key,
+    required this.currentIndex,
+    this.onItemSelected,
+  });
+
+  /// Индекс активного пункта.
+  final int currentIndex;
+
+  /// Коллбэк для смены вкладки (можно оставить null,
+  /// если экран не должен реагировать).
+  final ValueChanged<int>? onItemSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    final cs = Theme.of(context).colorScheme;
+
+    return BottomNavigationBar(
+      backgroundColor: Color.alphaBlend(
+        Colors.grey.withAlpha((0.2 * 255).round()),
+        cs.surface,
+      ),
+      selectedItemColor: AppColors.tertiary40,
+      unselectedItemColor: cs.outlineVariant,
+      type: BottomNavigationBarType.fixed,
+      currentIndex: currentIndex,
+      onTap: onItemSelected,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.menu_book), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: ''),
+      ],
+    );
+  }
+}
